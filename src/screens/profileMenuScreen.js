95052ef97a3f07701f8e949a27932a735e4d2566
KaushikBhadarka <kaushikbhadarka99@gmail.com>
2025-05-31 15:41:55 +0530

    Logout Modal added

--- src/screens/profileMenuScreen.js
+++ src/screens/profileMenuScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -11,11 +11,13 @@ import {
 } from "react-native";
 import { AppColor, Primary400, Secondary400 } from "../utils/theme";
 import { useDispatch, useSelector } from "react-redux";
+import Modal from "react-native-modal";
 import { onSignOut } from "../redux/slices/authSlice";
 import { clearUserSlice } from "../redux/slices/userSlice";
 import StatusBarManager from "../components/StatusBarManager";
 import FastImage from "@d11/react-native-fast-image";
 import Entypo from "react-native-vector-icons/Entypo";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import Feather from "react-native-vector-icons/Feather";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import CustomProfileItem from "../components/CustomProfileItem";
@@ -23,33 +25,71 @@ import LinearGradient from "react-native-linear-gradient";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const avatarImg = require("../assets/images/profileMenuActive.png");
-const dessertIcon = require("../assets/images/FT-Demo-01.png");
 const favTruck1 = require("../assets/images/FT-Demo-01.png");
 const favTruck2 = require("../assets/images/FT-Demo-02.png");
 
 const HR = () => <View style={styles.HR} />;
 
+const LogoutModal = ({ isModalVisible, onYesLogoutPress, onNoLogoutPress }) => (
+  <Modal
+    isVisible={isModalVisible}
+    backdropOpacity={0.5}
+    animationIn="zoomIn"
+    animationOut="zoomOut"
+  >
+    <View style={styles.modalContainer}>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        style={{ position: "absolute", top: 10, right: 10 }}
+        onPress={onNoLogoutPress}
+      >
+        <Ionicons
+          name="close-circle-sharp"
+          size={32}
+          color={AppColor.primary}
+        />
+      </TouchableOpacity>
+      <Text style={styles.modalTitle}>{"LoGoUT"}</Text>
+      <Text style={styles.modalSubtitle}>
+        {"Are you sure you want to logout?"}
+      </Text>
+      <TouchableOpacity
+        style={styles.logoutModalBtnYes}
+        activeOpacity={0.7}
+        onPress={onYesLogoutPress}
+      >
+        <Text style={styles.logoutModalBtnText}>{"Yes"}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.logoutModalBtnNo}
+        activeOpacity={0.7}
+        onPress={onNoLogoutPress}
+      >
+        <Text style={[styles.logoutModalBtnText, { color: AppColor.primary }]}>
+          {"No"}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  </Modal>
+);
+
 const ProfileMenuScreen = () => {
   const dispatch = useDispatch();
+  const [logoutModalVisible, setLogoutModalVisible] = useState(false);
+
   const { isSignedIn, isGuest } = useSelector((state) => state.authReducer);
   const { user } = useSelector((state) => state.userReducer);
   const insets = useSafeAreaInsets();
 
-  // Demo progress
   const ordersCompleted = 5;
   const totalOrders = 10;
   const progress = ordersCompleted / totalOrders;
 
-  const handleSignInOut = () => {
-    if (isSignedIn) {
-      dispatch(onSignOut());
-      dispatch(clearUserSlice());
-    } else {
-      dispatch(onSignOut());
-    }
+  const handleLogout = () => {
+    dispatch(clearUserSlice());
+    dispatch(onSignOut());
   };
 
-  // Demo favorite trucks
   const favoriteTrucks = [
     {
       id: 1,
@@ -194,7 +234,7 @@ const ProfileMenuScreen = () => {
             imageUri={require("../assets/images/logout.png")}
             label="Logout"
             rightIcon={false}
-            onPress={handleSignInOut}
+            onPress={() => setLogoutModalVisible(true)}
           />
           <HR />
           <CustomProfileItem
@@ -205,6 +245,14 @@ const ProfileMenuScreen = () => {
             onPress={() => {}}
           />
         </View>
+
+        {/* Modals */}
+        <LogoutModal
+          isModalVisible={logoutModalVisible}
+          onYesLogoutPress={handleLogout}
+          onNoLogoutPress={() => setLogoutModalVisible(false)}
+        />
+
         {/* Keep old logic for demo */}
         <View style={{ marginTop: 30, alignItems: "center" }}>
           {isSignedIn ? (
@@ -217,7 +265,7 @@ const ProfileMenuScreen = () => {
             >{`Hello, ${user?.firstName}`}</Text>
           ) : null}
           <TouchableOpacity
-            onPress={handleSignInOut}
+            onPress={handleLogout}
             activeOpacity={0.7}
             style={styles.signInButton}
           >
@@ -434,5 +482,63 @@ const styles = StyleSheet.create({
   HR: {
     height: 1,
     backgroundColor: "#E5E5EA",
+  },
+
+  // Logout Modal
+  modalContainer: {
+    backgroundColor: AppColor.white,
+    marginHorizontal: "5%",
+    paddingVertical: 36,
+    paddingHorizontal: 33,
+    borderRadius: 24,
+  },
+  modalTitle: {
+    marginBottom: 30,
+    fontSize: 22,
+    fontFamily: Primary400,
+    color: AppColor.text,
+    textAlign: "center",
+  },
+  modalSubtitle: {
+    marginBottom: 20,
+    fontSize: 16,
+    fontFamily: Secondary400,
+    color: AppColor.textHighlighter,
+    textAlign: "center",
+  },
+  logoutModalBtnYes: {
+    width: "100%",
+    height: 48,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: AppColor.primary,
+    marginTop: 15,
+    ...Platform.select({
+      ios: {
+        shadowColor: AppColor.black,
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 4,
+      },
+    }),
+  },
+  logoutModalBtnNo: {
+    width: "100%",
+    height: 48,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: AppColor.primary,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 15,
+  },
+  logoutModalBtnText: {
+    color: AppColor.white,
+    fontFamily: Secondary400,
+    fontSize: 16,
   },
 });
