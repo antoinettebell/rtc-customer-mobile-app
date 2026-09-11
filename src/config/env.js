73bf2263116292a34862d79105dde3cf0cee8b87
Round Da Corner <rounddacorner@MacBookPro.lan>
2026-09-11 01:51:35 -0400

    chore: use secure API domains

--- src/config/env.js
+++ src/config/env.js
@@ -8,10 +8,10 @@ const platformOS =
   NativeModules.PlatformConstants?.OS ??
   "android";
 
-const devApiUrl = "http://157.245.6.61:8000";
+const devApiUrl = "https://api-dev.rounddacornerapp.com";
 
 const fallbackConfig = {
-  API_URL: __DEV__ ? devApiUrl : "http://157.245.6.61:3000",
+  API_URL: __DEV__ ? devApiUrl : "https://api.rounddacornerapp.com",
   API_PREFIX: "/api/v1",
   GOOGLE_MAP_API_KEY: "AIzaSyCdjHeKOYBUcpocHw5-NsdfMy2-dHkBoFY",
   APPLE_PAY_MERCHANT_ID: "merchant.roundthecorner.vendor",
